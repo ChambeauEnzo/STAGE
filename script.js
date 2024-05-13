600661a840d286827649a4508a0e06620293f6b6
ChambeauEnzo <maryse.gosselin68@gmail.com>
2024-05-13 12:34:44 +0200

    PremierCommit

--- script.js
+++
@@ -1,9 +0,0 @@
-<script>
-    // Initialise la carte
-    var map = L.map('map').setView([51.505, -0.09], 13); // Centre la carte sur une position et définit le niveau de zoom initial
-
-    // Ajoute une couche de tuiles
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-</script>
