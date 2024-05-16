01325154723672e683534788db4b3ae9ce1c755b
ChambeauEnzo <maryse.gosselin68@gmail.com>
2024-05-16 09:51:06 +0200

    fondSM

--- script.js
+++ script.js
@@ -7,5 +7,25 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+//image fond//
 
+// Fonction pour ouvrir la modale avec une liste d'images
+function openModal(imageList) {
+    var modal = document.getElementById("modal");
+    var modalContent = document.getElementById("modalContent");
+    modalContent.innerHTML = ""; // Vide le contenu de la modale
+    imageList.forEach(function(imageSrc) {
+        var image = document.createElement("img");
+        image.src = imageSrc;
+        modalContent.appendChild(image);
+    });
+    modal.style.display = "block";
+    document.body.classList.add("blur"); // Ajouter une classe pour flouter le fond
+}
 
+// Fonction pour fermer la modale
+function closeModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "none";
+    document.body.classList.remove("blur"); // Retirer la classe pour enlever le flou
+}
