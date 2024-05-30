44dbc766bffd09334ab22988dba9fa07617e6079
ChambeauEnzo <maryse.gosselin68@gmail.com>
2024-05-30 09:55:04 +0200

    IconArrow

--- script.js
+++ script.js
@@ -75,5 +75,25 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollToTopButton = document.getElementById('scrollToTop');
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 200) {
+            scrollToTopButton.classList.add('show');
+            scrollToTopButton.classList.remove('hide');
+        } else {
+            scrollToTopButton.classList.add('hide');
+            scrollToTopButton.classList.remove('show');
+        }
+    });
+
+    scrollToTopButton.addEventListener('click', function () {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
 
 
